df42e34d0fda167d510fce6282c8a60c8041112e
Shalu Gupta <112753764+shalugupta5@users.noreply.github.com>
2023-08-05 23:50:26 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 # Import the create_app function from the 'app' module.
 from app import create_app
 
-# Call the create_app function to create our application.
 app = create_app()
 
-# Check if this script is being run directly (not imported as a module).
 if __name__ == "__main__":
     app.run()
