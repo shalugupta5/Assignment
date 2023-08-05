61cec89846ca21f99f60d9b47972f48213c84f20
shalu <shalug123456@gmail.com>
2023-08-05 20:09:55 +0530

    Folder name issue resolve

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = 'jdghkdfjgklfnknh'
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:shalu@localhost/brainerhub'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
